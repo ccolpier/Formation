fb2c15db1985e33a4ffb1ed570a901fe8453c592
ccolpier <c.colpier@dreamcentury.com>
2015-09-04 15:16:06 +0200

    ''

--- App/FrontEnd/FrontEndApplication.php
+++ App/FrontEnd/FrontEndApplication.php
@@ -2,6 +2,16 @@
 
 namespace \App\FrontEnd;
 
-class FrontEndApplication {
-    
+class FrontEndApplication extends \OCFram\Application {
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function run(){
+        $controller = $this->getController();
+        $controller->execute();
+
+        $this->HTTPResponse()->setPage($controller->page());
+        $this->HTTPResponse()->send();
+    }
 }
