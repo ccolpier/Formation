57713c42eb66b446eef0af8858931a45581ebd48
ccolpier <c.colpier@dreamcentury.com>
2015-09-04 12:12:34 +0200

    ''

---
+++ lib/OCFram/Router.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OCFram;
+
+class Router{
+    protected $routes = array();
+
+    public function addRoute(Route $route){
+        if(!in_array($route, $this->routes))
+            $this->routes[] = $route;
+    }
+
+    public function getRoute($url) {
+        foreach($this->routes as $route){
+            if($route->url = $url){
+                return $route;
+            }
+        }
+    }
+}
